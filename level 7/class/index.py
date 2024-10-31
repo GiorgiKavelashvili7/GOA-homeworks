247df9924cda4f834dc1f1db7e3f0b8cf19401ce
GiorgiKavelashvili7 <gkavelashvili85@gmail.com>
2024-10-31 19:46:25 +0400

    second commit

---
+++ level 7/class/index.py
@@ -0,0 +1,9 @@
+name = input("your name: ")
+surname = input("your surname: ")
+age = input("your age: ")
+country = input("your country: ")
+city = input("your city: ")
+faviorite_car = input("your faviorite car: ")
+faviorite_color = input("your faviorite color: ")
+print("My name is " + name + " " + surname + " and i am" + " " + str(age) + " years old" + " I live in " + country + "," + city + " my faviorite color is " + faviorite_car + " " + "and my faviorite color is " + faviorite_color)
+
