366c75332563092ac1cbeb67b6a9ba2b1e07f621
GiorgiKavelashvili7 <gkavelashvili85@gmail.com>
2024-11-03 19:25:00 +0400

    second commit

---
+++ Day 09/class/input.py
@@ -0,0 +1,9 @@
+# input() ინსტრუქციის დახმარებით შემოიტანეთ 3 სხვადასხვა მონაცემთა ტიპის მქონე მნიშვნელობა, შემდეგ კი შეამოწმეთ მათი მონაცემთა ტიპები.
+
+String = str(input("please input a string:" ))
+Integer = int(input("please input an integer:" ))
+Float = float(input("please input a float:"  ))
+
+print(type(String))
+print(type(Integer))
+print(type(Float))
---
+++ Day 09/home/user.py
@@ -0,0 +1,13 @@
+# მომხარებელს შემოატანინეთ მისი ასაკი, როგორც სტრინგი და დაბეჭდეთ მისი ტიპი. შემდეგ შეუცვალეთ მას მონაცემთა ტიპი ჯერ integer-ად, შემდეგ float-ად (ყოველ გარდაქმნაზე დაბეჭდეთ შედეგი)
+#---------------------------------------#
+
+User_Age = str (input("Please enter your age: "))
+print(type(User_Age))
+
+User_Age = int (input("Please enter your age: "))
+print(type(User_Age))
+
+User_Age = float (input("Please enter your age: "))
+print(type(User_Age))
+
+
