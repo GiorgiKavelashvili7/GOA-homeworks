09b1632b82d0adf896d540267eb1b87be843dd5f
GiorgiKavelashvili7 <gkavelashvili85@gmail.com>
2024-11-18 20:05:24 +0400

    commit

---
+++ Day 13/class/GOA.py
@@ -0,0 +1,4 @@
+Count = 0
+while Count < 50:
+   print("GOA")
+   Count = Count + 1
---
+++ Day 13/class/example.py
@@ -0,0 +1,7 @@
+# selection -  ზე, შექმენით 1 მაგალითი
+
+age = int(input("please input your age: "))
+if age >= 18:
+    print("You are an adult")
+elif age < 18:
+    print("You are a child")    
