fa2a64f3534e013111722995c2ade0391d3da352
GiorgiKavelashvili7 <gkavelashvili85@gmail.com>
2024-12-16 18:29:15 +0400

    commit

--- Day 19/home/hw2.py
+++ Day 19/home/hw2.py
@@ -1,5 +1,12 @@
 # მაქსიმალური ელემენტის პოვნა სიაში
 # დავალება: მომხმარებლის მიერ შეყვანილი რიცხვების სიაში იპოვეთ უდიდესი რიცხვი, გარდა ჩაშენებული ფუნქციისა (e.g. max).
 
-list = input("please input some numbers: ")
-print((list))
+numbers = list(map(int, input("შეიყვანეთ რიცხვები მძიმით გამოყოფილი").split(',')))
+if len(numbers) == 0:
+    print("სია ცარიელია")
+else:
+    max_number = numbers[0]
+    for num in numbers:
+        if num > max_number:
+            max_number = num
+    print(f"უდიდესი რიცხვი არის {max_number}")
