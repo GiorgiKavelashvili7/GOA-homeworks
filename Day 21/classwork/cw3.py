31c06d15715556b3afa191b14a2c02e64538258f
GiorgiKavelashvili7 <gkavelashvili85@gmail.com>
2024-12-21 21:37:28 +0400

    commit

---
+++ Day 21/classwork/cw3.py
@@ -0,0 +1,8 @@
+# You are working on queue manegement program.
+# The queue is represented by a list.
+# Write a program to take an input, add it to the end of the queue, and output the resulting list.
+# existing queue
+queue = ['John', 'Amy', 'Bob', 'Adam'] #  მოცემული გავქვს სია
+hiya = input("Please input a person's name: ") # კომპიუტერი მომხმარებელს სთხოვს პიროვნების სახელის შეტანას
+queue.append(hiya) # კომპიუტერი სიას ამატებს პიროვნების სახელს.
+print(queue) # კომპიუტერი იტანს სიის განახლებულ მნიშვნელობას.
