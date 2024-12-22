40bb8f083a83ee93be6fc76c9f6a9c1a24f8a3e1
GiorgiKavelashvili7 <gkavelashvili85@gmail.com>
2024-12-22 18:15:25 +0400

    commit

--- Day 21/homework/hw3.py
+++ Day 21/homework/hw3.py
@@ -1,4 +1,4 @@
-# Slicing and List Comprehensions Create a list named numbers that contains the integers from 1 to 10. Use list slicing to create a new list named first_half that contains the first five elements from numbers. Use list slicing to create another list named second_half that contains the last five elements from numbers. Use a list comprehension to create a new list named squares that contains the squares of each number in the numbers list. Print all three lists: first_half, second_half, and squares.
+# Slicing and List Comprehensions. Create a list named numbers that contains the integers from 1 to 10. Use list slicing to create a new list named first_half that contains the first five elements from numbers. Use list slicing to create another list named second_half that contains the last five elements from numbers. Use a list comprehension to create a new list named squares that contains the squares of each number in the numbers list. Print all three lists: first_half, second_half, and squares.
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 first_half = numbers[0:5]
--- Day 21/homework/hw4.py
+++ Day 21/homework/hw4.py
@@ -1,8 +1,14 @@
-# List Manipulation and Aggregation Create a list named temperatures that contains the following values representing daily temperatures: [72, 68, 75, 70, 78, 74, 71]. Calculate and print the highest temperature using the max() function. Calculate and print the lowest temperature using the min() function. Calculate and print the average temperature using the sum() function divided by the length of the list. Use a list comprehension to create a new list named above_70 that contains only the temperatures above 70 degrees. Print the above_70 list.
+# List Manipulation and Aggregation
+# Create a list named temperatures that contains the following values representing daily temperatures: [72, 68, 75, 70, 78, 74, 71].
+# Calculate and print the highest temperature using the max() function.
+# Calculate and print the lowest temperature using the min() function.
+# Calculate and print the average temperature using the sum() function divided by the length of the list.
+# Use a list comprehension to create a new list named above_70 that contains only the temperatures above 70 degrees.
+# Print the above_70 list.
 
 temperatures = [72, 68, 75, 70, 78, 74, 71]
 print(max(temperatures))
 print(min(temperatures))
 print(sum(temperatures)//len(temperatures))
-above_70 = [i > 70 for i in temperatures]
-print(above_70)
+Above_70 = [temperatures for temperatures in temperatures if temperatures > 70]
+print(Above_70)
