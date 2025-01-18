f6a01092f7e25d192c1106546e0de454004b1951
GiorgiKavelashvili7 <gkavelashvili85@gmail.com>
2025-01-18 23:28:18 +0400

    commit

---
+++ Day 29/class/cw2.py
@@ -0,0 +1,14 @@
+# Complete the task:
+# --------------------------
+
+# existing queue:
+queue = ["John", "Amy", "Bob", "Adam"] # მოცემული გვაქვს სია შემდეგი მნიშვნელობებით: John, Amy, Bob, Adam.
+
+# Take input from the user.
+input = input("Please input something: ") # კომპიუტერი მომხმარებელს სთოხვს რომ შემოიტანოს ტექსტი ტერმინალში.
+
+# Add the input item to the end of the queue.
+queue.append(input) # კომპიუეტერი ამატებს მომხმარებლის მიერ შემოტანილ ტექსტს სიის ბოლოში.
+
+# Output the resulting list.
+print(queue) # კომპიუეტერი იტანს სიას ტერმინალში.
---
+++ Day 29/home/hw2.py
@@ -0,0 +1,18 @@
+# Basic List Operations
+# Create a list named fruits that contains the following items: "apple", "banana", "cherry", "date", and "elderberry".
+# Print the entire list.
+# Access and print the first and last items in the list.
+# Add a new fruit "fig" to the list.
+# Remove "banana" from the list.
+# Change the value of the second item to "blueberry".
+# Print the length of the list.
+
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+print(fruits)
+print(fruits[0])
+print(fruits[4])
+fruits.append("fig")
+fruits.remove("banana")
+fruits[1] = "blueberry"
+print(len(fruits)) 
+
