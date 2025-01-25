f8a2e065e20e7498c43144c8ae53f415592968c6
GiorgiKavelashvili7 <gkavelashvili85@gmail.com>
2025-01-25 22:25:21 +0400

    commit

---
+++ Day 31/class/cw1.py
@@ -0,0 +1,7 @@
+# format() - გზის დახმარებით, დააფორმატეთ ტექსტი, შექმენით 3 ცვლადი, სახელი, გვარი და ასაკი, ეს ცვლადები ჩასვით ტექსტში, რომელიც იქნება თქვენს თავზე. მაგ: ჩემი სახელია ...  ჩემი გვარია ... და ჩემი ასაკია ....
+
+name = "Giorgi"
+surname = "Kavelashvili"
+age = 12
+
+information = f" My name is {name}, my surname is {surname} and my age is {age}"
