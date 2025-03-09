5df741dbec95a6ba8521ae3b65d2c90b1ade961c
GiorgiKavelashvili7 <gkavelashvili85@gmail.com>
2025-03-09 14:26:19 +0400

    Delete Day 42 directory

--- Day 42/class/cw2.py
+++
@@ -1,7 +0,0 @@
-"""
-Clock shows h hours, m minutes and s seconds after midnight.
-Your task is to write a function which returns the time since midnight in milliseconds.
-"""
-
-def past(h, m, s):
-    return h * 3600000 + m * 60000 + s * 1000 
